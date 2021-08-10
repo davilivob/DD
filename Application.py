7f2815f46c36255e668a0eedd7ab96bc7c909be8
dovilivob <david.iii.chiu@gmail.com>
2021-08-11 04:35:43 +0800

    Application 1.0.0

--- Application.py
+++ Application.py
@@ -1,10 +1,11 @@
 vocabularies = 0
 with open("daily_english.json", mode="r") as file:
     dictionary = file.read()
-dictionary = dictionary.replace('""', '').replace(' "', '').replace('."', '').replace('!"','').replace('?"', '')
+dictionary = dictionary.replace('": [', '~')
 
 for char in dictionary:
-    if char == '"':
+    if char == '~':
         vocabularies += 1
+
 # print(dictionary,'\n')
 print(vocabularies)
