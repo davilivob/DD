7973b3fd412d97391104407728dc4649db486364
dovilivob <david.iii.chiu@gmail.com>
2021-08-12 01:53:11 +0800

    Application 3.1

--- Application.py
+++ Application.py
@@ -1,10 +1,19 @@
 import string
+
 with open("daily_english.json", mode="r") as file:
     Dictionary = file.read()
     Words = Dictionary
+
 allEnglishChar = list(string.ascii_lowercase + string.ascii_uppercase + ' ')
 
 decimalDegree = 10
+
+BigSpace = " "*24
+SmallSpace = " "*15
+
+
+def fitSpace(string):
+    return " "*(32-len(string))
 
 
 class Print:
@@ -20,6 +29,9 @@ class Math:
         percentage = ((element)*100)/object
         percentage = float(int(percentage*degree))/degree
         return ' ( '+str(percentage)+'% )'
+
+    def takeDecimal(x, y, degree):
+        return float(int(x/y*degree))/degree
 
 
 class RegularData:
@@ -88,37 +100,86 @@ class AdvancedData:
         return WordsRepeated[index][1]
 
     TopUsed = ""
-    RankRange = 50
+    RankRange = 100
     for index in range(0, RankRange):
-        TopUsed += '"' + WordsItself(WordsRepeated, index)+'"'
-        TopUsed += ": " + str(WordsAmount(WordsRepeated, index))+" " + \
-            Math.percent(WordsAmount(WordsRepeated, index), len(
-                wordsList), decimalDegree) + ","
-        TopUsed += " "*(30-len(": " + str(WordsAmount(WordsRepeated, index))+" " +
-                                                     Math.percent(WordsAmount(WordsRepeated, index), len(
-                                                         wordsList), decimalDegree) + ","+'"' + WordsItself(WordsRepeated, index)+'"'))
-        if index % 5 == 4:
+        Word = '"' + WordsItself(WordsRepeated, index)+'"'
+        Amount = ": " + str(WordsAmount(WordsRepeated, index))+" "
+        Percentage = Math.percent(WordsAmount(WordsRepeated, index), len(
+                wordsList), decimalDegree)
+
+        TopUsed += Word + Amount + Percentage
+        TopUsed += fitSpace(Amount + Percentage + Word)
+        if index % 5 == 4 and index <= RankRange-3:
             TopUsed += "\n\n      "
 
 
 R = RegularData
 A = AdvancedData
-Print.ln('')
-Print.ln('VOCABULARIES: ' + str(R.voc))
-Print.tabln('Learned: ' + str(R.alr) +
-            Math.percent(R.alr, R.voc, decimalDegree))
-Print.tabln('Unlearned: ' + str(R.unl) +
-            Math.percent(R.unl, R.voc, decimalDegree))
-Print.ln('SENTENCES: ' + str(R.sen))
-Print.tabln('Examples: ' + str(R.exp) +
-            Math.percent(R.exp, R.sen, decimalDegree))
-Print.tabln('Definitions: ' + str(R.defi) +
-            Math.percent(R.defi, R.sen, decimalDegree))
-Print.tabln('Comparisons: ' + str(R.com) +
-            Math.percent(R.com, R.sen, decimalDegree))
-Print.ln("ADVANCED DATA: ")
-Print.tabln("Total words: " + str(len(A.wordsList)))
-Print.tabln("Top "+str(A.RankRange)+" used words: ")
-Print.tabln(A.TopUsed)
+class AllanPoeTest:
+    # Original goal is "eaoidhnrstuycfglmwbkpqxz"
+    Letter = A.WordsRepeated
+    letters = ''
+    LettersRepeated = []
+    for word in Letter:
+        letters += word[1]*word[0]
+    letters += 'ss'
+    allLowerLetter = list(string.ascii_lowercase)
+    for letter in allLowerLetter:
+        LettersRepeated.append([0, letter])
+    for index in range(0, len(LettersRepeated)):
+        for letter in letters:
+            if letter == LettersRepeated[index][1]:
+                LettersRepeated[index][0] += 1
 
-# Print.ln(str(A.WordsRepeated))
+    LettersRepeated.sort(reverse=True)
+    ShowLettersSort = ''
+    for letter in LettersRepeated:
+        ShowLettersSort += letter[1] + ", "
+    ShowLettersSort = ShowLettersSort.replace('z, ', 'z')
+
+    CurrentSortList = ShowLettersSort.split(', ')
+    CriticalSortList = ['e', 't', 'a', 'o', 'i', 'n', 's', 'h', 'r', 'd', 'l',
+                        'c', 'u', 'm', 'w', 'f', 'g', 'y', 'p', 'b', 'v', 'k', 'j', 'x', 'q', 'z']
+    Grade = 100
+    for char in CurrentSortList:
+        num = 0
+        dist = abs(CurrentSortList.index(char) - CriticalSortList.index(char))
+        num = 1-(1/25*dist)
+        Grade *= num
+    Grade = Math.percent(Grade, 100, 100)
+
+
+APT = AllanPoeTest
+
+Vocbularies = 'VOCABULARIES: ' + str(R.voc)
+Learned = 'Learned: ' + str(R.alr) + Math.percent(R.alr, R.voc, decimalDegree)
+Unlearned = 'Unlearned: ' + \
+    str(R.unl) + Math.percent(R.unl, R.voc, decimalDegree)
+Sentences = 'SENTENCES: ' + str(R.sen)
+Examples = 'Examples: ' + \
+    str(R.exp) + Math.percent(R.exp, R.sen, decimalDegree)
+Definitions = 'Definitions: ' + \
+    str(R.defi) + Math.percent(R.defi, R.sen, decimalDegree)
+Comparisons = 'Comparisons: ' + \
+    str(R.com) + Math.percent(R.com, R.sen, decimalDegree)
+TotalWords = "Total Words: " + str(len(A.wordsList))
+TotalCharacters = "Total Characters: " + str(len(APT.letters))
+LettersPerWord = "Letters Per Word: " + str(Math.takeDecimal(
+    len(APT.letters), len(A.wordsList), 1000))
+ATPrate = "Allan Poe Test: " + str(APT.Grade)[2:-1]
+ATPresult = "Result:     " + APT.ShowLettersSort
+Print.ln('')
+Print.ln(Vocbularies)
+Print.tabln(Learned + fitSpace(Learned) + Unlearned)
+Print.ln(Sentences)
+Print.tabln(Examples + fitSpace(Examples) +
+            Definitions + fitSpace(Definitions) + Comparisons)
+Print.ln("ADVANCED DATA: ")
+Print.tabln(TotalWords + fitSpace(TotalWords) + TotalCharacters +
+            fitSpace(TotalCharacters) + LettersPerWord)
+Print.ln("TOP "+str(A.RankRange)+" USED WORDS: ")
+Print.tabln(A.TopUsed)
+Print.ln(ATPrate)
+Print.tabln(
+    "Stantard:   e, t, a, o, i, n, s, h, r, d, l, c, u, m, w, f, g, y, p, b, v, k, j, x, q, z")
+Print.tabln(ATPresult)
