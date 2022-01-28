1d042a2b6d08af67a1ac01efe85336af46183670
dovilivob <david.iii.chiu@gmail.com>
2022-01-28 20:46:59 +0800

    contest

--- addwtj.py
+++ addwtj.py
@@ -1,4 +1,11 @@
+
+from MyModules import Aj
 import json
+# import sys
+# from os.path import dirname, abspath
+# d = dirname(dirname(abspath(__file__)))
+# sys.path.append(d)
+
 
 with open('./Databases/Database.json') as file:
     DBjson = json.loads(file.read())
@@ -8,19 +15,22 @@ with open('./Databases/words_to_join.json') as file:
     wtjjson = json.loads(file.read())
 
 with open('Databases/text.txt') as file:
-    string = file.read()
+    string = file.read().lower()
 
-deleted = list((',', '.', "'s", "'", ';', ':', '?'))
-string = string.replace('-', ' ').replace('\n', ' ')
+deleted = list(('-', '\n', ',', '.', ';', ':', '?'))
 for char in deleted:
-    string = string.replace(char, '')
+    string = string.replace(char, ' ')
 
+while '  ' in string:
+    string = string.replace('  ', ' ')
 
 arr = string.split(' ')
+
+Aj.SeparateAllShort(arr)
 newarr = []
 
 for element in arr:
-    if element.lower() not in words and element.lower() not in wtjjson:
+    if element.lower() not in words and element.lower() not in wtjjson and element not in newarr and "'" not in element:
         newarr.append(element.lower())
 
 print(newarr)
